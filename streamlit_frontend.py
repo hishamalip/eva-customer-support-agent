2037c5f73ba0fee394c811f85e3ce2ba5f84f3c2
Hisham Ali <your.email@example.com>
2026-07-17 17:40:59 +0000

    initial commit

---
+++ streamlit_frontend.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+st.set_page_config(layout="wide", page_title="Flower Shop Chatbot", page_icon="🌻")
+left_column, middle_column, right_column = st.columns(3, border=True)
+
+# Initialize session state for message history
+if "message_history" not in st.session_state:
+    st.session_state.message_history = [{"content": "Hi, I'm Eva! A flower shop assistant. How can I assist you today?", "type": "assistant"}]
+
+# Clear button to reset the message history
+with left_column:
+    if st.button("❌ Clear", key="clear_chat"):
+        st.session_state.message_history = [{"content": "Hi, I'm Eva! A flower shop assistant. How can I assist you today?", "type": "assistant"}]
+
+
+# Display the chat interface in the middle column
+with middle_column:
+    st.header("🌻 Eva")
+
+    user_input = st.chat_input("Type your message here...")
+
+    if user_input:
+        st.session_state.message_history.append(
+            {"content": user_input, "type": "user"}
+        )
+
+    for message in (st.session_state.message_history)[::-1]:
+        # st.write(message["content"])
+        message_box = st.chat_message(message['type'])
+        message_box.markdown(message["content"])
+
+
+# Display the message history in the right column
+with right_column:
+    st.header("Message History:")
+    st.text(st.session_state.message_history)
+
+
